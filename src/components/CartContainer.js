5807b8657e7238de2b69ddc6b49481de4d3c8d8d
Elliott Delano Wynn III <delanowynn@yahoo.com>
2022-11-17 18:00:46 -0500

    adding async fetch

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,6 @@
 import CartItem from './CartItem'
 import {useDispatch, useSelector} from 'react-redux'
-import {clearCart} from '../features/cart/cartSlice'
+import {openModal} from '../features/modal/modalSlice'
 function CartContainer(){
     const dispatch = useDispatch();
     const {cartItems, total, amount} = useSelector((store) => store.cart)
@@ -26,10 +26,10 @@ function CartContainer(){
             <hr />
             <div className='cart-total'>
                 <h4>
-                    total <span>${total}</span>
+                    total <span>${total.toFixed(2)}</span>
                 </h4>
             </div>
-            <button className='btn clear btn' onClick={() => dispatch(clearCart())}>clear cart</button>
+            <button className='btn clear btn' onClick={() => dispatch(openModal())}>clear cart</button>
         </footer>
     </section>
 };
